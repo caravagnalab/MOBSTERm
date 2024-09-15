6ba92132ea4ccacc51307671c8263e53323fb5be
Elena Rivaroli <elena.rivaroli@edu.unife.it>
2024-09-15 10:38:15 +0200

    fixed plot functions

--- utils/plot_functions.py
+++ utils/plot_functions.py
@@ -13,7 +13,12 @@ import seaborn as sns
 
 def plot_deltas(mb):
     deltas = mb.params["delta_param"].detach().numpy()
-    fig, ax = plt.subplots(nrows=deltas.shape[0], ncols=1)
+    if deltas.shape[0] == 1:
+        fig, ax = plt.subplots(nrows=deltas.shape[0], ncols=1, figsize=(6, 1.5))  # Custom size for 1 plot
+    else:
+        fig, ax = plt.subplots(nrows=deltas.shape[0], ncols=1)
+    if deltas.shape[0] == 1:
+        ax = [ax]  # add an extra dimension to make it 2D
     fig.tight_layout() 
     for k in range(deltas.shape[0]):
         sns.heatmap(deltas[k], ax=ax[k], vmin=0, vmax=1, cmap="crest")
@@ -33,7 +38,11 @@ def plot_responsib(mb):
 
 def plot_paretos(mb):
     alpha_pareto = mb.params["alpha_pareto_param"].detach().numpy()
-    fig, ax = plt.subplots(nrows=alpha_pareto.shape[0], ncols=alpha_pareto.shape[1])
+    if alpha_pareto.shape[0] == 1:
+        fig, ax = plt.subplots(nrows=alpha_pareto.shape[0], ncols=alpha_pareto.shape[1], figsize = (7,3))
+        ax = np.array([ax])
+    else:
+        fig, ax = plt.subplots(nrows=alpha_pareto.shape[0], ncols=alpha_pareto.shape[1])      
     fig.tight_layout()
     x = np.arange(0,0.5,0.001)
     for k in range(alpha_pareto.shape[0]):
@@ -46,8 +55,11 @@ def plot_paretos(mb):
 def plot_betas(mb):
     phi_beta = mb.params["phi_beta_param"].detach().numpy()
     kappa_beta = mb.params["k_beta_param"].detach().numpy()
-
-    fig, ax = plt.subplots(nrows=phi_beta.shape[0], ncols=phi_beta.shape[1])
+    if phi_beta.shape[0] == 1:
+        fig, ax = plt.subplots(nrows=phi_beta.shape[0], ncols=phi_beta.shape[1], figsize = (7,3))
+        ax = np.array([ax])
+    else:
+        fig, ax = plt.subplots(nrows=phi_beta.shape[0], ncols=phi_beta.shape[1])   
     fig.tight_layout()
     x = np.arange(0,1,0.001)
     for k in range(phi_beta.shape[0]):
@@ -58,46 +70,8 @@ def plot_betas(mb):
             ax[k,d].plot(x, pdf, 'r-', lw=1)
             ax[k,d].set_title(f"Sample {d+1} Cluster {k} - phi {round(float(phi_beta[k,d]), ndigits=2)}, kappa {round(float(kappa_beta[k,d]), ndigits=2)}", fontsize=10)
 
+
 def plot_marginals(mb):
-    delta = mb.params["delta_param"]  # K x D x 2
-    phi_beta = mb.params["phi_beta_param"].detach().numpy()
-    kappa_beta = mb.params["k_beta_param"].detach().numpy()
-    alpha = mb.params["alpha_pareto_param"].detach().numpy()
-    weights = mb.params["weights_param"].detach().numpy()
-    labels = mb.params['cluster_assignments'].detach().numpy()
-
-    # For each dimension, for each cluster, we need to plot the density corresponding to the beta or the pareto based on the value of delta
-    fig, axes = plt.subplots(1, 2, figsize=(14, 6))
-    x = np.linspace(0.001, 1, 1000)
-    for d in range(mb.NV.shape[1]):
-        for k in range(mb.K):
-            delta_kd = delta[k, d]
-            maxx = torch.argmax(delta_kd)
-            if maxx == 1:
-                # plot beta
-                a = phi_beta[k,d] * kappa_beta[k,d]
-                b = (1-phi_beta[k,d]) * kappa_beta[k,d]
-                pdf = beta.pdf(x, a, b)# * weights[k]
-                axes[d].plot(x, pdf, linewidth=1.5, label='Beta', color='r')
-            else:
-                #plot pareto
-                pdf = pareto.pdf(x, alpha[k,d], scale=0.01)# * weights[k]
-                axes[d].plot(x, pdf, linewidth=1.5, label='Pareto', color='g')
-        axes[d].legend()
-        data = mb.NV[:,d].numpy()/mb.DP[:,d].numpy()
-        # cmap = plt.get_cmap('viridis', np.unique(labels))
-        for i in np.unique(labels):
-            axes[d].hist(data[labels == i], density=True, bins=30, alpha=0.5)#, color=cmap(i))
-
-        # axes[d].hist(data[labels == 0], density=True, bins=30, alpha=0.3, color='violet')
-        # axes[d].hist(data[labels == 1], density=True, bins=30, alpha=0.3, color='yellow')
-        axes[d].set_title(f"Sample {d+1}")
-        # axes[d].set_ylim([0,100])
-        axes[d].set_xlim([0,1])
-        plt.tight_layout()
-
-
-def plot_marginals2(mb):
     delta = mb.params["delta_param"]  # K x D x 2
     phi_beta = mb.params["phi_beta_param"].detach().numpy()
     kappa_beta = mb.params["k_beta_param"].detach().numpy()
@@ -108,7 +82,12 @@ def plot_marginals2(mb):
     # For each sample I want to plot all the clusters separately.
     # For each cluster, we need to plot the density corresponding to the beta or the pareto based on the value of delta
     # For each cluster, we want to plot the histogram of the data assigned to that cluster
-    fig, axes = plt.subplots(mb.NV.shape[1], mb.K, figsize=(15, 6))
+    if mb.K == 1:
+        fig, axes = plt.subplots(mb.NV.shape[1], mb.K, figsize=(6, 5))
+    else:
+        fig, axes = plt.subplots(mb.NV.shape[1], mb.K, figsize=(15, 6))
+    if mb.K == 1:
+        axes = axes[:, None]  # add an extra dimension to make it 2D
     x = np.linspace(0.001, 1, 1000)
     for d in range(mb.NV.shape[1]):
         for k in range(mb.K):
@@ -133,3 +112,41 @@ def plot_marginals2(mb):
             # axes[d,k].set_ylim([0,100])
             axes[d,k].set_xlim([0,1])
             plt.tight_layout()
+
+# def plot_marginals(mb):
+#     delta = mb.params["delta_param"]  # K x D x 2
+#     phi_beta = mb.params["phi_beta_param"].detach().numpy()
+#     kappa_beta = mb.params["k_beta_param"].detach().numpy()
+#     alpha = mb.params["alpha_pareto_param"].detach().numpy()
+#     weights = mb.params["weights_param"].detach().numpy()
+#     labels = mb.params['cluster_assignments'].detach().numpy()
+
+#     # For each dimension, for each cluster, we need to plot the density corresponding to the beta or the pareto based on the value of delta
+#     fig, axes = plt.subplots(1, 2, figsize=(14, 6))
+#     x = np.linspace(0.001, 1, 1000)
+#     for d in range(mb.NV.shape[1]):
+#         for k in range(mb.K):
+#             delta_kd = delta[k, d]
+#             maxx = torch.argmax(delta_kd)
+#             if maxx == 1:
+#                 # plot beta
+#                 a = phi_beta[k,d] * kappa_beta[k,d]
+#                 b = (1-phi_beta[k,d]) * kappa_beta[k,d]
+#                 pdf = beta.pdf(x, a, b)# * weights[k]
+#                 axes[d].plot(x, pdf, linewidth=1.5, label='Beta', color='r')
+#             else:
+#                 #plot pareto
+#                 pdf = pareto.pdf(x, alpha[k,d], scale=0.01)# * weights[k]
+#                 axes[d].plot(x, pdf, linewidth=1.5, label='Pareto', color='g')
+#         axes[d].legend()
+#         data = mb.NV[:,d].numpy()/mb.DP[:,d].numpy()
+#         # cmap = plt.get_cmap('viridis', np.unique(labels))
+#         for i in np.unique(labels):
+#             axes[d].hist(data[labels == i], density=True, bins=30, alpha=0.5)#, color=cmap(i))
+
+#         # axes[d].hist(data[labels == 0], density=True, bins=30, alpha=0.3, color='violet')
+#         # axes[d].hist(data[labels == 1], density=True, bins=30, alpha=0.3, color='yellow')
+#         axes[d].set_title(f"Sample {d+1}")
+#         # axes[d].set_ylim([0,100])
+#         axes[d].set_xlim([0,1])
+#         plt.tight_layout()
